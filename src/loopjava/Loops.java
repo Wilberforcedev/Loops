e7ad1b80b27384bd88e9bd35a6b84464fceb0bd6
kyagulanyi allan <allankyagulanyi8@gmail.com>
2024-10-06 19:14:30 +0300

    my first upload

---
+++ src/loopjava/Loops.java
@@ -0,0 +1,13 @@
+package loopjava;
+
+public class Loops {
+
+	public static void main(String[] args) {
+		// Loop to return odd numbers
+				for (int y=1; y<=15; y+=2) {
+		                 System.out.println(y);
+				
+				}
+			 
+			}
+		}
